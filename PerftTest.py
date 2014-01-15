1180658d197e958e83acdc54677346f01a8d4bfe
jackliddle <jackliddlephysics@googlemail.com>
2014-01-15 22:31:01 +0100

    Caught up with my code again, need to implement castling

--- PerftTest.py
+++ PerftTest.py
@@ -8,7 +8,7 @@ from LegalMoves import getBoards,isInCheck
 from Board import Board
 
 maxdepth = 3
-test_positions = range(6)#[0]
+test_positions = [1]#range(6)#[0]
 
 
 
@@ -16,21 +16,22 @@ def testPosition(FEN,perftdata):
     b1 = Board()
     b1.initFromFEN(FEN)
     print FEN
-    print '%3s%12s%24s%24s'%('','Nodes','Captures','Checks')
+    print '%3s%12s%24s%24s%24s'%('','Nodes','Captures','Checks','Castles')
     have_need = '%12s%12s'%('Have','Need')
-    have_need *= 3
-    print '%3s%1s'%('',have_need)
+    have_need *= 4
+    print '%4s%1s'%('',have_need)
     for depth in range(maxdepth):
         
         nextboards,nextmoves = getBoards(b1,depth+1)
         Ncaps = len( filter(lambda(i): i == 'CAPTURE',nextmoves) )
+        Ncast = len( filter(lambda(i): i == 'CASTLE' ,nextmoves) )
         try:
             checks = [isInCheck(board) for board in nextboards]
             checks = [check for check in checks if len(check)]
             Nchecks = len(checks)
         except:
             Nchecks = "ILLEGAL"
-        print '%3s%12s%12s%12s%12s%12s%12s'%( depth,len(nextboards),perftdata[depth].Nodes,Ncaps,perftdata[depth].Captures,Nchecks,perftdata[depth].Checks)
+        print '%3s%12s%12s%12s%12s%12s%12s%12s%12s'%( depth,len(nextboards),perftdata[depth].Nodes,Ncaps,perftdata[depth].Captures,Nchecks,perftdata[depth].Checks,Ncast,perftdata[depth].Castles  )
     print
 
 
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Writing a Chess AI in Python.
 
 Currently passes [Perft position 1 upto depth 3](http://chessprogramming.wikispaces.com/Perft+Results)
 
+15/01/2014:  Need to implement Castling into the move generation
